5f46f433e1b9132dcafb04607f6fccd606adf571
w40141 <w.40141@gmail.com>
2022-01-29 00:17:44 +0900

    edit

--- 0088_Merge_Sorted_Array/solution.py
+++ 0088_Merge_Sorted_Array/solution.py
@@ -1,12 +1,25 @@
+from typing import List
+
+
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        nums2 = nums2 + [0 for _ in range(len(nums1) - n)]
-        ans = []
-        s = 0
-        t = 0
-        while m > 0 and n > 0:
-            if :
-                
+        end = m
+        for i in range(m + n):
+            print(i, nums1, nums2, end)
+            if len(nums1) == 0:
+                nums1[i] = nums2.pop(0)
+                end += 1
+            elif len(nums2) == 0:
+                continue
+            elif nums1[i] > nums2[0]:
+                nums1.insert(i, nums2.pop(0))
+                nums1.pop()
+                end += 1
+            elif nums1[i] == 0 and i == end:
+                nums1[i] = nums2.pop(0)
+                end += 1
+            else:
+                continue
