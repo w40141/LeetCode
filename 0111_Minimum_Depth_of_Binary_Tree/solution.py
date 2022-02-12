8c2ddf572638e7c847ee6d715d22e683373a0356
w40141 <w.40141@gmail.com>
2022-02-12 22:43:13 +0900

    edit

--- 0111_Minimum_Depth_of_Binary_Tree/solution.py
+++ 0111_Minimum_Depth_of_Binary_Tree/solution.py
@@ -11,23 +11,15 @@ class TreeNode:
 class Solution:
     def minDepth(self, root: Optional[TreeNode]) -> int:
         def depth(root: TreeNode, height: int) -> int:
-            flag = 1
-            if root.left is None:
-                flag += 1
-                height_left = height
-            else:
-                height_left = depth(root.left, height + 1)
-
-            if root.right is None:
-                flag += 1
-                height_right = height
-            else:
-                height_right = depth(root.right, height + 1)
-
-            if flag == 2:
+            if root.left is None and root.right is None:
                 return height
             else:
-                return min(height_left, height_right)
+                d = 1000000
+                if root.left is not None:
+                    d = min(d, depth(root.left, height + 1))
+                if root.right is not None:
+                    d = min(d, depth(root.right, height + 1))
+                return d
 
         if root is None:
             return 0
